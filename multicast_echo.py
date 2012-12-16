74e9c359d0d814420f194f9bf7c7fadd15370b16
nirvik <nirvik1993@gmail.com>
2012-12-16 16:30:34 +0530

    multicast.py
    class containing a custom exception class and "Multicast" class....

--- multicast_echo.py
+++ multicast_echo.py
@@ -1,28 +1,67 @@
-#!/usr/bin/python
 import socket as sck
-from struct import pack
-import sys
-mcast_grp=('224.4.28.87',500) # tupule consisting of multicast ip and port no
-message='hey ... waiting for response....'#the message to be sent
-x=sck.socket(sck.AF_INET,sck.SOCK_DGRAM) #creating socket AF_INET family :- socket datagram
-sck.settimeout(0.1)
-time=pack('b',1) #pack 1 in the given format and control no of networks which will recieve the packet
-x.setsockopt(sck.IPPROTO_IP,sck.IP_MULTICAST_TTL,time)# setting up socket options..
-try:
-	print 'sending message  ',message
-	sent=x.sendto(message,mcast_grp) #send message to the multicast ip
-	while 1:
-		print 'waiting for responses'
-		try:
-			data,server=x.recvfrom(16)
-			#print 'connection received from ',server
-		except sck.timeout:
-			print 'time exceeded..closing socket'
-			x.close()
-			sys.exit(0)
-		else :
-			print 'received a message : {0} from {1}'.format(data,server) 
-finally:
-	print 'closing socket!'
-	x.close()
+import logging
+mcast_ip='224.23.23.29'
+port=9878
+timeout=5
+buff_size=1024
+logging.root.setLevel(logging.INFO)
 
+class ConnectingError(Exception):
+        def __init__(self,value,mesg):
+                self.val=value
+                self.mesg=mesg
+
+        def __str__(self):
+                return repr(self.mesg)
+
+class multicast(sck.socket):
+    def __init__(self,timeout):
+
+        super(multicast,self).__init__(sck.AF_INET,sck.SOCK_DGRAM,sck.IPPROTO_UDP)
+        self.setsockopt(sck.IPPROTO_IP,sck.IP_MULTICAST_TTL,2)
+        self.setsockopt(sck.SOL_SOCKET,sck.SO_REUSEADDR,1)
+        self.setsockopt(sck.SOL_IP, sck.IP_MULTICAST_LOOP, 1)
+        self.bind(('',port))
+        ip=raw_input("whats ur ip:")
+        self.setsockopt(sck.SOL_IP,sck.IP_MULTICAST_IF,sck.inet_aton(ip))
+        self.setsockopt(sck.SOL_IP,sck.IP_ADD_MEMBERSHIP,sck.inet_aton(mcast_ip)+sck.inet_aton(ip))
+        self.settimeout(timeout)
+        logging.info("socket initialised ")
+
+    def send_mes(self):
+        self.sendto("Broadcasting! looking for peers\n",(mcast_ip,port))
+        logging.info("BROADCASTING \n")
+    def listen(self):
+        d=ConnectingError(1,"no response")
+        try:
+            (data,conn)=self.recvfrom(buff_size)
+            print 'got connection from {0}'.format(conn)
+            logging.info("connection received")
+        except sck.timeout:
+                raise d
+                logging.info("SOCKET TIMED OUT ! therefore no response\n")
+
+    def discover_bcast_mesg(self):
+        a=ConnectingError(2,"exceeded no of tries")
+
+        n=0 #no of attempts
+        tries=3
+        while 1:
+                self.send_mes()
+                if(n>tries):
+                        raise a
+                        self.close()
+                        break
+
+                else:
+                        try:
+                                self.listen()
+                                self.close()
+                                break
+                        except ConnectingError as c:
+                                if c.val==1:
+                                        logging.warning("FAILED {0}".format(n+1))
+                                        n+=1
+                                        pass
+                                else:
+        				raise c                                                                                                                            67,3-24       93%
