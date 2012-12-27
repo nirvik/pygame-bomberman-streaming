2a13a8f14416f2744f46885a91ccd36e681da1dd
nirvik <nirvik1993@gmail.com>
2012-12-27 23:34:02 +0530

    displaying the confirmed players on the screen along with ip's

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ from multicasting import *
 class start(multicast):
 
 	def __init__(self):
-		super(self,start).__init__(timeout)	
+		super(start,self).__init__(timeout)	
 		self.stream_message=[]
 		try:
 			self.discover_bcast_mesg()
@@ -22,20 +22,42 @@ class start(multicast):
 		pygame.init()
 		self.scr=pygame.display.set_mode((700,700))
 		pygame.display.set_caption('Project Bomberman')
-		load=pygame.image.load('loading.png').convert()
-		self.scr.blit(load,(0,0))
-		pygame.display.flip()
+		self.load=pygame.image.load('loading.png').convert()
+		#self.scr.blit(load,(0,0))
+		#pygame.display.flip()
 		self.join_game()
 
 
 	def join_game(self):
 		print 'for the options input like this :p1 (if opt=P1) '
-		which_player=raw_input('Choose Player \n p1 \n p2 \n p3 \n p4:')
+		which_player=raw_input('Choose Player \n p1 \n p2 \n p3 \n p4:')#since all the comps are goint to be together,players can discuss amongst themselves and discuss which player no they want 
 		id=which_player[-1]
 		string=self.uid+' '+id
-		self.players_uids[self.uid]=id
-		self.communication(string)
+		self.players_ids[self.uid]=id
+		try:
+			self.communication(string)
+		except ConnectingError as c:
+			if c.val==2:
+				print c.mesg
+				pass
+		#pygame.display.flip()
+		self.coordinate1=350
+		self.coordinate2=0
+		#INITIALISING FONTS ---PYGAME 
+		font=pygame.font.Font(None,20)
+		
 		print 'displaying the uids and player ids\n'
 		for keys in self.players_ids:
-			print keys,':',self.players_ids[keys]	
+			text=font.render("{0} is player {1}".format(keys,self.players_ids[keys]),1,(255,255,255))
+			textpos=text.get_rect(center=(self.coordinate1,self.coordinate2))
+			self.coordinate2+=50
+			textpos.centerx=self.load.get_rect().centerx
+			self.load.blit(text,textpos)
+		
+		self.scr.blit(self.load,(0,0))
+		pygame.display.flip()
 	
+	def __del__(self):
+		self.close()
+		logging.info("Closing socket")
+
