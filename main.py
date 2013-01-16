219917cc2c37ab572a4b0af2ee393d044f5ad96d
nirvik <nirvik1993@gmail.com>
2013-01-17 00:28:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python
-<<<<<<< HEAD
-
-from game import *
-
-def main():
-	
-	a=tic_tac_toe()
-	a.run()
-=======
 from game import *
 
 def main():
 
         a=tic_tac_toe()
         a.run()
->>>>>>> 59b52af7939453f8afcebcf124d0f64a00ff5b4c
+
 
 if __name__=='__main__':
         main()
-~                                                                                                                                                     
-~                                                                                                                                                     
-~                     
